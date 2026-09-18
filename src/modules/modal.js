b63175b8d77350be35cf4974f1763c8842a31463
shukina7olga <olga.shchukina.99@mail.ru>
2026-09-19 01:33:37 +0300

    animation modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,14 +3,40 @@ const modal = () => {
     const closeBtn = modal.querySelector('.popup-close')
     const buttons = document.querySelectorAll('.popup-btn')
 
+    const isMobile = window.innerWidth < 768
+
+    console.log(window.innerWidth)
+
+    if (!isMobile) {
+        modal.style.opacity = '0'
+        modal.style.transition = 'opacity 0.9s'
+    }
+
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = 'block'
+            
+            if(!isMobile) {
+                //чтобы мы увидели как отрабатывает строка, а не все сразу выполнилось
+                requestAnimationFrame(() => {
+                    modal.style.opacity = '1'
+                })
+            }
         })
     })
 
     closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none'
+        if (!isMobile) {
+            modal.style.opacity = '0'
+        } else {
+            modal.style.display = 'none'
+        }
+    })
+
+    modal.addEventListener('transitionend', () => {
+        if (modal.style.opacity === '0') {
+            modal.style.display = 'none'
+        }
     })
 }
 
