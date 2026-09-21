d18d8712fcc79ae0900ff2b5a3d223520550d09e
shukina7olga <olga.shchukina.99@mail.ru>
2026-09-22 00:13:08 +0300

    delegation in modal and tabs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import forms from "./modules/forms";
+import tabs from "./modules/tabs";
 
 timer('25 september 2026')
 menu()
 modal()
 forms()
+tabs()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,16 +1,23 @@
 const modal = () => {
     const modal = document.querySelector('.popup')
-    const closeBtn = modal.querySelector('.popup-close')
     const buttons = document.querySelectorAll('.popup-btn')
 
     const isMobile = window.innerWidth < 768
-
-    console.log(window.innerWidth)
 
     if (!isMobile) {
         modal.style.opacity = '0'
         modal.style.transition = 'opacity 0.9s'
     }
+
+    modal.addEventListener('click', (e) => {
+        if (!e.target.closest('.popup-content') || e.target.closest('.popup-close')) {
+            if (!isMobile) {
+                modal.style.opacity = '0'
+            } else {
+                modal.style.display = 'none'
+            }
+        }
+    })
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -25,13 +32,6 @@ const modal = () => {
         })
     })
 
-    closeBtn.addEventListener('click', () => {
-        if (!isMobile) {
-            modal.style.opacity = '0'
-        } else {
-            modal.style.display = 'none'
-        }
-    })
 
     modal.addEventListener('transitionend', () => {
         if (modal.style.opacity === '0') {
@@ -41,3 +41,49 @@ const modal = () => {
 }
 
 export default modal
+
+
+
+
+
+// const modal = () => {
+//     const modal = document.querySelector('.popup')
+//     const closeBtn = modal.querySelector('.popup-close')
+//     const buttons = document.querySelectorAll('.popup-btn')
+
+//     const isMobile = window.innerWidth < 768
+
+//     if (!isMobile) {
+//         modal.style.opacity = '0'
+//         modal.style.transition = 'opacity 0.9s'
+//     }
+
+//     buttons.forEach(btn => {
+//         btn.addEventListener('click', () => {
+//             modal.style.display = 'block'
+            
+//             if(!isMobile) {
+//                 //чтобы мы увидели как отрабатывает строка, а не все сразу выполнилось
+//                 requestAnimationFrame(() => {
+//                     modal.style.opacity = '1'
+//                 })
+//             }
+//         })
+//     })
+
+//     closeBtn.addEventListener('click', () => {
+//         if (!isMobile) {
+//             modal.style.opacity = '0'
+//         } else {
+//             modal.style.display = 'none'
+//         }
+//     })
+
+//     modal.addEventListener('transitionend', () => {
+//         if (modal.style.opacity === '0') {
+//             modal.style.display = 'none'
+//         }
+//     })
+// }
+
+// export default modal
