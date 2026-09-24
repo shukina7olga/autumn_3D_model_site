ab04a709b70a588b302212879442ba6c6ac7adf8
shukina7olga <olga.shchukina.99@mail.ru>
2026-09-24 21:33:12 +0300

    delegation

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,23 +1,24 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu')
-    const menu = document.querySelector('menu')
-    const closeBtn = document.querySelector('.close-btn')
-    const menuItems = menu.querySelectorAll('ul>li>a')
+    const menuBlock = document.querySelector('menu')
 
-    const handleMenu = () => {
-        // if (!menu.style.transform) {
-        //     menu.style.transform = `translateX(0)`
-        // } else {
-        //     menu.style.transform = ``
-        // }   
-        menu.classList.toggle('active-menu')
-    }
+    menuBtn.addEventListener('click', (e) => {
+        e.stopPropagation()
+        menuBlock.classList.toggle('active-menu')
+    })
 
-    menuBtn.addEventListener('click', handleMenu)
+    document.addEventListener('click', (e) => {
+        const isMenuOpen = menuBlock.classList.contains('active-menu')
 
-    closeBtn.addEventListener('click', handleMenu)
+        if (e.target.closest('.close-btn') || e.target.closest('menu ul > li > a')) {
+            menuBlock.classList.remove('active-menu')
+            return
+        }
 
-    menuItems.forEach(item => item.addEventListener('click', handleMenu));
+        if (isMenuOpen && !e.target.closest('menu')) {
+            menuBlock.classList.remove('active-menu')
+        }
+    })
 }
 
 export default menu
