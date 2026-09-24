b05e6dc2079c50cda4b5c5ca8564098b48f1b63d
shukina7olga <olga.shchukina.99@mail.ru>
2026-09-24 23:28:55 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import forms from "./modules/forms";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer('25 september 2026')
 menu()
 modal()
 forms()
 tabs()
+slider()
